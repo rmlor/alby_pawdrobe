143f24beaa66aa930992b1141e72b61ba1c8c84c
Nhu Dang <122351051+ndangu@users.noreply.github.com>
2024-11-19 03:02:56 -0800

    Update app.js to complete step 3
    http://localhost:4189/ verifies working handlebars
    http://localhost:4189/Order_Products verifies connected database

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 /*
     SETUP
 */
-var express = require('express');   // We are using the express library for the web server
-var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 4189;                 // Set a port number at the top so it's easy to change in the future
-var db = require('./database/db-connector')
+var express = require('express');                   // We are using the express library for the web server
+var app     = express();                            // We need to instantiate an express object to interact with the server in our code
+PORT        = 4189;                                 // Set a port number at the top so it's easy to change in the future
+var db = require('./database/db-connector')         // Connecting to database
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');         // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));      // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                     // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
 /*
     ROUTES
 */
+// app.js
+
+app.get('/', function(req, res)
+    {
+        res.render('index');                        // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                             // will process this file, before sending the finished HTML to the client.
 
 app.get('/Order_Products', function(req, res) {     //Fetch Order_Products
     // Define queries
