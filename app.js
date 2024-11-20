1e684c21dca3c62ea23fb81c4d707a64c0ec038b
rmlor <lor.ruthm@gmail.com>
2024-11-20 10:55:27 -0800

    added customer page

--- app.js
+++ app.js
@@ -3,6 +3,12 @@
 */
 
 var express = require('express');                   // We are using the express library for the web server
+
+/*
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+*/
 var app     = express();                            // We need to instantiate an express object to interact with the server in our code
 PORT        = 4189;                                 // Set a port number at the top so it's easy to change in the future
 var db = require('./database/db-connector')         // Connecting to database
@@ -94,7 +100,18 @@ app.get('/products', function(req, res)             //Fetch Products
             res.json(products); 
         });
     });
-    
+
+    //Customers page
+    app.get('/customers', function(req, res)
+    {  
+        let query1 = "SELECT * FROM Customers;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('customers', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                
+    });           
+
     //Add a product to an order
     app.post('/orders/:id/products/add', function(req, res) 
     {
@@ -110,6 +127,7 @@ app.get('/products', function(req, res)             //Fetch Products
             res.redirect(`/orders`);
         });
     });
+
     
 /*
     LISTENER
